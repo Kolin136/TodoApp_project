ec09b92db2f1f9e4182104b4534433996aa6dbfd
Kolin136 <jsowb136@gmail.com>
2023-11-13 20:15:07 +0900

    토큰검사 제외card관련  서비스 구현

--- src/main/java/com/todoapp/todoapp/repository/TodoAppRepository.java
+++ src/main/java/com/todoapp/todoapp/repository/TodoAppRepository.java
@@ -3,5 +3,8 @@ package com.todoapp.todoapp.repository;
 import com.todoapp.todoapp.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoAppRepository extends JpaRepository<Card,Long> {
+    List<Card> findAllByOrderByCreateAtDesc();
 }
--- src/main/java/com/todoapp/todoapp/service/TodoAppService.java
+++ src/main/java/com/todoapp/todoapp/service/TodoAppService.java
@@ -2,6 +2,7 @@ package com.todoapp.todoapp.service;
 
 import com.todoapp.todoapp.dto.CardRequestDto;
 import com.todoapp.todoapp.dto.CardResponseDto;
+import com.todoapp.todoapp.entity.Card;
 import com.todoapp.todoapp.repository.TodoAppRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,19 +18,43 @@ public class TodoAppService {
     private final TodoAppRepository todoAppRepository;
 
     public CardResponseDto createCard(CardRequestDto requestDto) {
+        CardResponseDto cardResponseDto = new CardResponseDto(new Card(requestDto));
 
-
+        return cardResponseDto;
     }
 
+    @Transactional(readOnly = true)
     public CardResponseDto getIdCard(Long id) {
+
+        CardResponseDto cardResponseDto = new CardResponseDto(findCard(id));
+
+        return cardResponseDto;
     }
 
+    @Transactional(readOnly = true)
     public List<CardResponseDto> getCards() {
+        return todoAppRepository.findAllByOrderByCreateAtDesc()
+                .stream().map(CardResponseDto::new).toList();
     }
 
     public CardResponseDto updateCard(Long id, CardRequestDto requestDto) {
+
+        Card card = findCard(id);
+        card.update(requestDto);
+
+        return new CardResponseDto(card);
     }
 
     public void deleteCard(Long id) {
+        Card card = findCard(id);
+        todoAppRepository.delete(card);
     }
+
+    private Card findCard(Long id) {
+        return todoAppRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 카드는 없습니다")
+        );
+    }
+
+
 }
