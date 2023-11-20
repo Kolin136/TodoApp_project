866ddec3c03b329193e13490a6386d2ddf16d9c4
Kolin136 <jsowb136@gmail.com>
2023-11-20 21:08:02 +0900

    JwtAuthorizationFilter 주석 삭제

--- src/main/java/com/todoapp/todoapp/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/todoapp/todoapp/jwt/JwtAuthorizationFilter.java
@@ -35,16 +35,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
         String tokenValue = jwtUtil.getJWtHeader(req);
 
-
-//        if (!StringUtils.hasText(tokenValue) || !jwtUtil.validateToken(tokenValue)) {
-//            log.error("Token Error");
-//            res.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 상태 보내기
-//            res.setContentType("application/json");
-//            res.setCharacterEncoding("utf-8");
-//            PrintWriter writer = res.getWriter();
-//            writer.println("토큰이 유효하지 않습니다.");
-//            return;
-//        }
         if (StringUtils.hasText(tokenValue)){
 
             if (!jwtUtil.validateToken(tokenValue)) {
@@ -68,18 +58,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 return;
             }
         }
-//        else {
-//            log.info("Token null");
-//            res.setContentType("application/json");
-//            res.setCharacterEncoding("utf-8");
-//            PrintWriter writer = res.getWriter();
-//            writer.println("토큰이 없는 상태입니다.");
-//        }
-
-        // 과제 필수사항에 "토큰이 필요한 API 요청에서 토큰을 전달하지 않았거나 정상 토큰이 아닐 때는 "토큰이 유효하지 않습니다." 라는 에러메시지와 statusCode: 400을 Client에 반환하기"
-        // 시큐리티 필터가 JwtAuthentizationFilter -> JwtAuthenticaionFilter 순으로 하도록 config에서 설정해서 토큰 생성전 로그인 하려할때도 JwtAuthentizationFilter를 걸치니
-        // JwtAuthentizationFilter에서 토큰이 비어있을시 오류 메세지 보내는게 문제..
-        // 일단 현재 해결 방법은 모르겠고 토큰 비어있을시 메세지 보내면 회원가입쪽이랑 충돌 이어나서 주석 처리합니다.
 
         filterChain.doFilter(req, res);
     }
